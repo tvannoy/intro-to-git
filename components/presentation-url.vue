5166ac9a2a5ce64bc353acbc824fdff861adcbd7
Trevor Vannoy <trevorvannoy@montana.edu>
2023-10-17 09:36:27 -0600

    format vue component code

--- components/presentation-url.vue
+++ components/presentation-url.vue
@@ -1,18 +1,14 @@
-<script>
-
-export default {
-  data: () => {
-      return {
-        href: window.location.origin + window.location.pathname,
-        url: window.location.host + window.location.pathname
-      };
-  },
-}
-
-
-
-</script>
-
 <template>
-<a :href="href">{{ url }}</a>
+  <a :href="href">{{ url }}</a>
 </template>
+
+<script>
+  export default {
+    data() {
+        return {
+          href: window.location.origin + window.location.pathname,
+          url: window.location.host + window.location.pathname
+        };
+    },
+  }
+</script>
